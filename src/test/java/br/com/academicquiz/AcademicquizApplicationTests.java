ed90656d1841fff256bed9dafdc2d8f45a2ff9d1
emersonjesuz <josiemerson2013@gmail.com>
2024-09-12 11:12:29 -0300

    test: remover test

---
+++ src/test/java/br/com/academicquiz/AcademicquizApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.academicquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
