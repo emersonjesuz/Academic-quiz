0e28a0111f976522598c0ab4ca8a83ea08f5e895
emersonjesuz <josiemerson2013@gmail.com>
2024-09-12 11:13:50 -0300

    feat: add theme model

---
+++ src/main/java/br/com/academicquiz/theme/models/ThemeModel.java
@@ -0,0 +1,57 @@
+package br.com.academicquiz.theme.models;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import br.com.academicquiz.question.models.QuestionModel;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "themes")
+public class ThemeModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "theme", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonManagedReference
+    private Set<QuestionModel> questions = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<QuestionModel> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(Set<QuestionModel> questions) {
+        this.questions = questions;
+    }
+
+}
